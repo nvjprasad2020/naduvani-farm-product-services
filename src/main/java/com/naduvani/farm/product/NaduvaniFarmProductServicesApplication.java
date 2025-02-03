1e76ea264a8429a37830ddf6228d1d1a6a2c481c
nvjprasad <nvjprasad2020@gmail.com>
2025-02-03 03:28:25 +0000

    Initial commit - added project structure

---
+++ src/main/java/com/naduvani/farm/product/NaduvaniFarmProductServicesApplication.java
@@ -0,0 +1,13 @@
+package com.naduvani.farm.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaduvaniFarmProductServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaduvaniFarmProductServicesApplication.class, args);
+	}
+
+}
